3b69381026615a7962fb060d1daad7803b0639c1
Anton Zahrabantsau <anton.zagrebancev@gmail.com>
2022-08-21 17:01:11 +0500

    Add documentation.

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # SQLiteDataPicker
 
-## Instructions
+## Description
 
-We would like you to write a Python program which queries an SQLite database and writes data to a JSON file. You would need to download the database from [here](https://github.com/lerocha/chinook-database/blob/master/ChinookDatabase/DataSources/Chinook_Sqlite.sqlite). 
-You can find the SQL script used to populate the database [here](https://raw.githubusercontent.com/lerocha/chinook-database/master/ChinookDatabase/DataSources/Chinook_Sqlite.sql).
+This is a Python program which queries an SQLite database and writes data to a JSON file. 
 
-The program should query customer invoice payments, with the option to filter for specific time periods, and write the output to a JSON file in this format:
+You can download the database from [here](https://github.com/lerocha/chinook-database/blob/master/ChinookDatabase/DataSources/Chinook_Sqlite.sqlite).<br>
+Or you can find the SQL script used to populate the database [here](https://raw.githubusercontent.com/lerocha/chinook-database/master/ChinookDatabase/DataSources/Chinook_Sqlite.sql).
+
+The program queries customer invoice payments, with the option to filter for specific time periods, and write the output to a JSON file in format presented below.<br>
+The output customers sorted by `total_paid` amount in descending order.<br>
+The JSON filename include the timestamp of when the script was executed.
 
 ```json
 [
@@ -48,18 +52,62 @@ The program should query customer invoice payments, with the option to filter fo
     ...
 ]
 ```
-In the output the customers should be sorted by `total_paid` amount in descending order. The JSON filename should include the timestamp of when the script was executed. Last, we would also like to see some tests for your program (you can decide what functionalities are the most important to test).
-
-We have provided the command line interface for the program (but you may wish to validate user input). You may use any 3rd party packages you wish. If you choose to do so, please add instructions on how to install them.
-
-
 
 ## Dependencies
 
-- Python 3
+The program works stable with **Python 3.8** or higher.<br>
+All third-party dependencies listed in `requirements.txt` file in core directory.
 
-
-## How to run
+## How to work with program
+### Install dependencies:
 ```bash
-python main.py [--start YYYY-MM-DD] [--end YYYY-MM-DD]
+make install
 ```
+### Run tests:
+```bash
+make test
+```
+You can set up the test database more advanced using `.test.env` file.<br>
+See the end of the section for more information about the available settings. 
+
+### Run script:
+```bash
+python main.py [--start YYYY-MM-DD] [--end YYYY-MM-DD] [--path path/for/output/file]
+```
+You can set up the database more advanced using `.local.env` file.<br>
+See the end of the section for more information about the available settings. 
+
+### DB advanced setup:
+- DB_URL: string; database address
+- DB_DRIVER: string; Python DB driver, which will be used by SQLAlchemy to interact with the DB
+- DB_ECHO: bool; if set to `true` each executed query will be logged in console.
+
+## Comments regarding implementation
+### Tech stack
+SQLAlchemy ORM is used in the program for interacting with the database.
+
+It is one of the most used Python ORMs with big open-source community.<br>
+It is distributed under an open free MIT license and available both private and commercial use.<br>
+We also can be sure that the library will be kept up to date, including timely security updates, bugs and technical issues, compatibility with related packages and Python versions.
+
+From technical perspective it provides well readable and easy-to-use interface for building queries, managing transaction/sessions, etc.<br>
+We will be able to quickly deliver updates or fixes, introduce custom metrics or logs, set up hooks for specific actions.<br>
+Interacting with DB through the SQLAlchemy interfaces also allows us to set up program to work with different DBMS with no need to take care of SQL dialects.
+Important to mention that rarely used SQLAlchemy feature - `automap_base` - gives us lot of flexibility in this specific case: 
+instead of building appropriate models manually we are just generating them by analyzing provided database.
+### Optimization
+While implementing a data-collecting or data-analyzing program we have to make sure that it will be able to work with big and very big amounts of data without significant issues.
+
+To keep it there were introduced a few mechanisms in the `CustomerPaymentsDataService`.
+
+#### Batched load:
+We are not loading whole data at once. Instead, program querying configurable number of records, process them and repeat the process until nothing will be left.<br>
+Use `batch_size` parameter of `CustomerPaymentsDataService._get_data_generator` method to configure number of records. Default value is 10000.
+
+#### Data as a generator:
+The program also does not store whole data in its memory. It uses Python generator to return data rows one by one from queried batch. 
+When the batch ends, the generator requests a new one seamlessly and continues to return data.
+
+Unfortunately, internal Python `json` package does not support dumping data from a generator. 
+Therefore, we are using `simplejson` package instead.<br>
+If `iterable_as_array=True` parameter is provided to the `simplejson.dump` method it retrieves rows one by one and immediately write them directly to target file without clogging RAM.
--- services/customer_payments_data_service.py
+++ services/customer_payments_data_service.py
@@ -22,6 +22,18 @@ class CustomerPaymentsDataService:
         end_date: Optional[datetime] = None,
         path: str = None
     ) -> None:
+        """Creates JSON file with customers with the list of their invoices.
+
+        If customer does not have invoices under the conditions passed, it will not be
+         added to the result selection.
+
+        :param start_date: Select only invoices billed at <start_date> or later.
+                            If None, parameter will be ignored without adding a filter.
+        :param end_date: Select only invoices billed earlier than <end_date>.
+                          If None, parameter will be ignored without adding a filter.
+        :param path: Path to directory where output file will be saved
+        :return: None
+        """
         data = self._get_data_generator(start_date, end_date)
         file_path = self._get_file_path(path)
 
@@ -35,6 +47,22 @@ class CustomerPaymentsDataService:
         end_date: Optional[datetime] = None,
         batch_size: int = 10000
     ) -> Generator[dict, None, None]:
+        """Returns the results of _get_customers_data method as a generator.
+
+        The method is created by optimization reasons.
+        It allows to avoid memory overflow and db connection timeouts by retrieving
+         data sample sequentially in batches.
+        Each batch is retrieved by separate request and fully loaded into memory.
+
+        :param start_date: start_date parameter for a _get_customers_data method.
+                            Passing directly.
+        :param end_date: end_date parameter for a _get_customers_data method.
+                          Passing directly.
+        :param batch_size: number of instances in one batch.
+        :return: Generator returning SQLAlchemy Row instances that contains two fields:
+                   Customer - instance of Customer model with appropriate data
+                   total_paid - Decimal value. Sum of selected invoices
+        """
         offset = 0
 
         while True:
@@ -60,6 +88,22 @@ class CustomerPaymentsDataService:
         batch_size: int,
         offset: int
     ) -> List[Row]:
+        """Retrieves customers with the list of their invoices.
+
+        If customer does not have invoices under the conditions passed, it will not be
+         added to the result selection.
+
+        :param start_date: Select only invoices billed at <start_date> or later.
+                            If None, parameter will be ignored without adding a filter.
+        :param end_date: Select only invoices billed earlier than <end_date>.
+                          If None, parameter will be ignored without adding a filter.
+        :param batch_size: SQL limit param
+        :param offset: SQL offset param
+        :return: List of SQLAlchemy Row instances that contains two fields:
+                           Customer - instance of Customer model with appropriate data
+                           total_paid - Decimal value. Sum of selected invoices
+        """
+
         customers_subquery = self._get_customers_subquery(
             start_date=start_date,
             end_date=end_date,
@@ -92,6 +136,19 @@ class CustomerPaymentsDataService:
         batch_size: int,
         offset: int
     ) -> Subquery:
+        """Generate subquery that calculates total amount of invoices for each customer.
+
+        :param start_date: Select only invoices billed at <start_date> or later.
+                            If None, parameter will be ignored without adding a filter.
+        :param end_date: Select only invoices billed earlier than <end_date> .
+                          If None, parameter will be ignored without adding a filter.
+        :param batch_size: SQL limit param
+        :param offset: SQL offset param
+        :return: Subquery containing two fields:
+                   CustomerId - id of a customer
+                   total_paid - total amount of invoices for appropriate customer
+        """
+
         base_queryset = (
             self._session.query(Customer)
             .join(Customer.invoice_collection)
@@ -115,22 +172,46 @@ class CustomerPaymentsDataService:
         )
 
     @staticmethod
-    def _data_row_to_dict(customer: Row) -> dict:
+    def _data_row_to_dict(data_row: Row) -> dict:
+        """Map data row of specific format to JSON serializable dictionary.
+    
+        :param data_row: SQLAlchemy Row instance that contains two fields:
+                           Customer - instance of Customer model with appropriate data
+                           total_paid - Decimal value. Sum of selected invoices
+        :return: JSON serializable dictionary
+        """
+
         return {
-            "customer_id": customer.Customer.CustomerId,
-            "first_name": customer.Customer.FirstName,
-            "last_name": customer.Customer.LastName,
-            "total_paid": str(customer.total_paid),
+            "customer_id": data_row.Customer.CustomerId,
+            "first_name": data_row.Customer.FirstName,
+            "last_name": data_row.Customer.LastName,
+            "total_paid": str(data_row.total_paid),
             "individual_payments": [
                 {
                     "date": str(invoice.InvoiceDate),
                     "amount": str(invoice.Total)
 
-                } for invoice in customer.Customer.invoice_collection
+                } for invoice in data_row.Customer.invoice_collection
             ]
         }
 
     def _get_file_path(self, path: str, copy: int = 0) -> str:
+        """Generate file name for output file and join it to provided path.
+
+        The file name will be "customer_payments_data_<YYYY-MM-DD_HH-MM>.json"
+        where the value in triangle brackets is current UTC date and time in appropriate
+        format.
+
+        If file with the same name already exists the method will add "(1)" to file name.
+        If such a file also exists too, it will increment number in brackets until free
+        name will be found.
+
+        :param path: Path to directory where output file will be saved
+        :param copy: Number, that should be added to file name. 0 value means
+                      nothing will be added.
+        :return: Path to output file
+        """
+
         file_name = f"customer_payments_data_{datetime.utcnow().strftime('%Y-%m-%d_%H-%M')}"
         if copy:
             file_name += f"({copy})"
